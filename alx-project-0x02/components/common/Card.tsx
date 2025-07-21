1857ecd05b0cb970908445cf7ece1e1e847f637a
manuel4life <allotey4eva@gmail.com>
2025-07-21 19:47:26 +0200

    type error

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,4 +1,5 @@
-import { CardProps } from "../../interfaces/index";
+import { type CardProps } from "@/interfaces";
+
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-6 m-4">
